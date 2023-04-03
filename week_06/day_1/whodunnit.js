3e11bbb0d44591e2386449abdd82e1794a2cae3c
McMuds <clairemcmurdo@duck.com>
2023-04-03 16:36:11 +0100

    Attempt 1 of homework (un-run)

---
+++ week_06/day_1/whodunnit.js
@@ -0,0 +1,202 @@
+// Episode 1:
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+//"The murderer is Miss Scarlet" - simple declaration - no trixieness
+
+// Episode 2:
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+//will fail to run, as you're attempting to change the murderer, which is a constant.
+
+//Episode 3:
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// "First Verdict: 'The murderer is Mrs. Peacock'" - it returns the local instance of murderer within the function
+// "Second Verdict: 'The murderer is Professor Plum" - this is the global murderer that's unchanged.
+
+// Episode 4:
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+// suspects = ['Miss Scarlet', 'Professor Plum', 'Colonel Mustard'] - locally suspect3 is changed inside the function
+//"Suspect three is Mrs Peacock" - this is unchanged outside the function.
+
+
+// Episode 5:
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+
+//"The weapon is the revolver" - the weapon inside the constant scenario is altered. the scenario (container) is not.
+
+
+// Episode 6:
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+//"The murderer is Mrs White" - change murderer changes the let murderer from outside become Mr Green. Plot Twist then 
+// changes that to be Mrs White.
+
+
+// Episode 7:
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+//"The murderer is Mr Green"
+// originally it's Mr Green, which is global. Then we declare LET murderer which is changed to Miss Scarlet, but it's not 
+// the same variable as the one that Mr Green is.
+
+
+// Episode 8:
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+
+//The weapon is Candle Stick - change scenario alters to be the dining room. Then plot twist says if it's the 
+// dining room, change the murderer to colonel mustard. Then unexpected outcome says if it's colonel mustard, change
+// the weapon to candlestick.
+
+// Episode 9:
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// the murderer is professor plum.
+// Despite the let murderer = Mrs Peacock line, that's a different instance of the local variable 
+// murderer as indicated by the let keyword 
