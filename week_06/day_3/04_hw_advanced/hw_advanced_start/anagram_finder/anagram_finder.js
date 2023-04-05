fd1d78deb60f366a34596adadafd2ed4fcbb616c
McMuds <clairemcmurdo@duck.com>
2023-04-05 22:33:32 +0100

    Not. A. Clue. Added PseudoCode. Too. Damn. Hard.

--- week_06/day_3/04_hw_advanced/hw_advanced_start/anagram_finder/anagram_finder.js
+++ week_06/day_3/04_hw_advanced/hw_advanced_start/anagram_finder/anagram_finder.js
@@ -1,9 +1,29 @@
 const AnagramFinder = function (word) {
-
+  this.word = word.split('');
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+  const result = otherWords.foreach((word) => {
+    const result2 = word.every((char) => this.isAnagram
 
+      }
+    });
+  })
+
+  });
 }
+
+AnagramFinder.prototype.isAnagram = function () {
+  const result = this.alphabet.every((char) => {
+    return this.phrase.toLowerCase().includes(char);
+  });
+  return result;
+};
+
+// No idea - what I want to do is:
+// for each word in the Array, do a count comparison of the two words.
+// If equal, forEach letter in the AnagramFinder word, remove each 
+// letter(firstIndexOf) in the Array word. If there's anything left in the 
+// Array.word after the AnagramFinder word, then return false.
 
 module.exports = AnagramFinder;
