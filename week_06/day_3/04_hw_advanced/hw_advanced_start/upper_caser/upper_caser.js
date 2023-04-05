a14a77494ed707f6200d7d3dd7ee69affdbff1aa
McMuds <clairemcmurdo@duck.com>
2023-04-05 19:41:23 +0100

    UpperCaser done

--- week_06/day_3/04_hw_advanced/hw_advanced_start/upper_caser/upper_caser.js
+++ week_06/day_3/04_hw_advanced/hw_advanced_start/upper_caser/upper_caser.js
@@ -1,8 +1,12 @@
 const UpperCaser = function (words) {
+  this.words = words;
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
-}
+  const result = this.words.map((char) => {
+    return char.toUpperCase()
+  });
+  return result;
+};
 
 module.exports = UpperCaser;
