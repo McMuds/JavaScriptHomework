b2767b25af4f0b1e259fcd49672b9fc902030580
McMuds <clairemcmurdo@duck.com>
2023-04-05 21:34:08 +0100

    This. Sucks. Never in a month of sundays. Not a chance.

--- week_06/day_3/04_hw_advanced/hw_advanced_start/isogram_finder/isogram_finder.js
+++ week_06/day_3/04_hw_advanced/hw_advanced_start/isogram_finder/isogram_finder.js
@@ -1,9 +1,13 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+  return this.word.every(char =>  this.isUnique(char));
 }
+
+IsogramFinder.prototype.isUnique = function(char) {
+  return (this.word.indexOf(char) === this.word.lastIndexOf(char));
+};
 
 module.exports = IsogramFinder;
