ed75f53842509efcdd3dd4469bbe8efec5b22473
McMuds <clairemcmurdo@duck.com>
2023-04-05 20:32:47 +0100

    JFC that wasn't easy. Panagram nonsense.

--- week_06/day_3/04_hw_advanced/hw_advanced_start/pangram_finder/pangram_finder.js
+++ week_06/day_3/04_hw_advanced/hw_advanced_start/pangram_finder/pangram_finder.js
@@ -1,9 +1,13 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
-
-}
+  const result = this.alphabet.every((char) => {
+    return this.phrase.toLowerCase().includes(char);
+  });
+  return result;
+};
 
 module.exports = PangramFinder;
